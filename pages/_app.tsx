e26d5d0ebdee5cbb5315fc88de921e72428376e9
Dario Djuric <dario@thisdot.co>
2022-11-25 22:25:30 +0100

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,24 @@ export default function App({ Component, pageProps }: AppProps) {
           content="Personal blog of Dario Djuric, a software engineer based in Zagreb, Croatia."
         />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-2W1599NQM6"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-2W1599NQM6', {
+page_path: window.location.pathname,
+});
+`,
+        }}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   );

34e6d3ead065aa6c1f4413d5d2c0240df00c97f6
Dario Djuric <dario@thisdot.co>
2022-11-25 19:50:26 +0100

    Add intro text

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <ChakraProvider><Component {...pageProps} /></ChakraProvider>
+  return (
+    <ChakraProvider>
+      <Head>
+        <title>Dario&apos;s Blog</title>
+        <meta
+          name="description"
+          content="Personal blog of Dario Djuric, a software engineer based in Zagreb, Croatia."
+        />
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }

15c9023e31942ec19dfa906a2ed16ec2c9c7ccb9
Dario Djuric <dario@thisdot.co>
2022-12-04 21:58:50 +0100

    Remove Facebook, Instagram, Medium

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,15 +8,7 @@ import {
   VStack,
 } from '@chakra-ui/react';
 import Link from 'next/link';
-import {
-  FaFacebook,
-  FaGithub,
-  FaInstagram,
-  FaLinkedin,
-  FaMastodon,
-  FaMedium,
-  FaTwitter,
-} from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaMastodon, FaTwitter } from 'react-icons/fa';
 
 const ICON_SIZE = 7;
 
@@ -40,15 +32,6 @@ export default function Home() {
             </Link>
             <Link href="https://twitter.com/dario_djuric">
               <Icon as={FaTwitter} w={ICON_SIZE} h={ICON_SIZE} />
-            </Link>
-            <Link href="https://dario-djuric.medium.com/">
-              <Icon as={FaMedium} w={ICON_SIZE} h={ICON_SIZE} />
-            </Link>
-            <Link href="https://www.instagram.com/dariodjuric/">
-              <Icon as={FaInstagram} w={ICON_SIZE} h={ICON_SIZE} />
-            </Link>
-            <Link href="https://www.facebook.com/dario.djuric/">
-              <Icon as={FaFacebook} w={ICON_SIZE} h={ICON_SIZE} />
             </Link>
             <Link href="https://mastodon.social/@dariodjuric">
               <Icon as={FaMastodon} w={ICON_SIZE} h={ICON_SIZE} />

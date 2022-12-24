f5348c6687b0d057ec4e8611c45c6ffc758a79eb
Dario Djuric <dario@thisdot.co>
2022-12-24 09:58:47 +0100

    Escape quotes

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,7 +4,10 @@ import { PageSEO } from '@/components/SEO';
 export default function FourZeroFour() {
   return (
     <>
-      <PageSEO title="Page Not Found" description="Sorry we couldn't find this page :(" />
+      <PageSEO
+        title="Page Not Found"
+        description="Sorry we couldn't find this page :("
+      />
       <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
         <div className="space-x-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
@@ -13,7 +16,7 @@ export default function FourZeroFour() {
         </div>
         <div className="max-w-md">
           <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-            Sorry we couldn't find this page.
+            Sorry we couldn&apos;t find this page.
           </p>
           <p className="mb-8">
             But dont worry, you can find plenty of other things on our homepage.
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,14 +18,19 @@ export const getStaticProps = async () => {
   return { props: { posts } };
 };
 
-export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home({
+  posts,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
-      <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      <PageSEO
+        title={siteMetadata.title}
+        description={siteMetadata.description}
+      />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Hi, I'm Dario
+            Hi, I&apos;m Dario
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
@@ -42,7 +47,9 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
                     <dl>
                       <dt className="sr-only">Published on</dt>
                       <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+                        <time dateTime={date}>
+                          {formatDate(date, siteMetadata.locale)}
+                        </time>
                       </dd>
                     </dl>
                     <div className="space-y-5 xl:col-span-3">

102a1948031fe1f43b9fc5285f02491d5056a4d4
Dario Djuric <dario@thisdot.co>
2024-03-02 20:48:24 +0100

    Update redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: () => {
+    const redirectMap = {
+      '/blog/2023-05-06-why-doesnt-autofill-work-on-my-form':
+        '/posts/why-doesnt-autofill-work-on-my-form',
+      '/blog/2022-12-22-adding-cognito-user-via-api':
+        '/posts/adding-cognito-user-via-api',
+      '/blog/2023-01-28-sentry-integration-woes-with-nextjs-flyio':
+        '/posts/sentry-integration-woes-with-nextjs-flyio',
+    };
+
+    return Object.keys(redirectMap).map((source) => ({
+      source,
+      destination: redirectMap[source],
+      permanent: true,
+    }));
+  },
+};
 
 module.exports = nextConfig;

ae53bb2965d3e15ea80edc9d9af1f609c9405aec
Dario Djuric <dario.djuric@gmail.com>
2024-11-11 16:04:25 +0100

    Add Bluesky

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,28 +1,26 @@
-import Image from 'next/image';
-import githubIcon from '@/public/icons/github.svg';
-import linkedinIcon from '@/public/icons/linkedin.svg';
-import xIcon from '@/public/icons/x.svg';
+import { FaBluesky, FaGithub, FaLinkedin, FaXTwitter } from 'react-icons/fa6';
 
 export default function Socials() {
   return (
-    <div className="self-center flex flex-row space-x-2">
+    <div className="self-center flex flex-row space-x-3">
       <a
         href="https://github.com/dariodjuric"
         target="_blank"
         className="opacity-100 hover:opacity-95"
       >
-        <Image priority src={githubIcon} alt="My GitHub profile" />
+        <FaGithub
+          className="h-full text-brand-content-inverse drop-shadow-[2px_2px_0px_rgba(0,0,0,0.15)]"
+          aria-label="My GitHub profile"
+        />
       </a>
       <a
         href="https://www.linkedin.com/in/dario-djuric"
         target="_blank"
         className="opacity-100 hover:opacity-95"
       >
-        <Image
-          priority
-          src={linkedinIcon}
-          alt="My LinkedIn profile"
-          className="fill-amber-300"
+        <FaLinkedin
+          className="h-full text-brand-content-inverse drop-shadow-[2px_2px_0px_rgba(0,0,0,0.15)]"
+          aria-label="My LinkedIn profile"
         />
       </a>
       <a
@@ -30,7 +28,20 @@ export default function Socials() {
         target="_blank"
         className="opacity-100 hover:opacity-95"
       >
-        <Image priority src={xIcon} alt="My X profile" />
+        <FaXTwitter
+          className="h-full text-brand-content-inverse drop-shadow-[2px_2px_0px_rgba(0,0,0,0.15)]"
+          aria-label="My X profile"
+        />
+      </a>
+      <a
+        href="https://bsky.app/profile/dariodjuric.bsky.social"
+        target="_blank"
+        className="opacity-100 hover:opacity-95"
+      >
+        <FaBluesky
+          className="h-full text-brand-content-inverse drop-shadow-[2px_2px_0px_rgba(0,0,0,0.15)]"
+          aria-label="My Bluesky profile"
+        />
       </a>
     </div>
   );

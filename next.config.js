80d8375bc83a959c614b3c6f100eb79fdd32b4b7
Dario Djuric <dario.djuric@gmail.com>
2025-08-17 17:34:25 +0200

    Add PostHog

--- next.config.js
+++ next.config.js
@@ -16,6 +16,26 @@ const nextConfig = {
       permanent: true,
     }));
   },
+
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/flags',
+        destination: 'https://eu.i.posthog.com/flags',
+      },
+    ];
+  },
+
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 module.exports = nextConfig;
